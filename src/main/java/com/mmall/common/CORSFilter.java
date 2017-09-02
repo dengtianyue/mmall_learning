f7031466dd4ba6fdf078d4df584c84b75f30fa04
dengtianyue <17855823710@163.com>
2017-09-03 01:43:00 +0800

    Crossorigin

---
+++ src/main/java/com/mmall/common/CORSFilter.java
@@ -0,0 +1,37 @@
+package com.mmall.common;
+
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+public class CORSFilter implements Filter {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token");
+        response.setHeader("Access-Control-Expose-Headers", "*");
+
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(200);
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+
+    }
+}
