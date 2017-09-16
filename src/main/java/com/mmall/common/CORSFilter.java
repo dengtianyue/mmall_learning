0fa18ca950b9a1fc32b67cdb01075df1083a8002
dengtianyue <17855823710@163.com>
2017-09-16 23:12:45 +0800

    update front

--- src/main/java/com/mmall/common/CORSFilter.java
+++ src/main/java/com/mmall/common/CORSFilter.java
@@ -18,7 +18,9 @@ public class CORSFilter implements Filter {
 //        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 //        response.setHeader("Access-Control-Max-Age", "3600");
 //        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        String originHeader = request.getHeader("Origin");
+        //response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token");
